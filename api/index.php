25acb7d4dba4e2cc8cb2622ba8bb703273c06a00
Wildan024 <muhammadwildanf24@gmail.com>
2026-09-17 10:41:51 +0700

    fix index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,16 @@
 <?php
 
+// 1. Arahkan folder cache Blade & Storage ke folder /tmp Vercel
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+
+// 2. Buat folder tmp jika belum ada
+if (!is_dir('/tmp/views')) {
+    mkdir('/tmp/views', 0755, true);
+}
+
+// 3. Jalankan aplikasi Laravel
 require __DIR__ . '/../public/index.php';
