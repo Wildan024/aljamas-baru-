f08165acb4837c7b7b000c8ce17dd73e823c9f47
Wildan024 <muhammadwildanf24@gmail.com>
2026-09-17 10:52:33 +0700

    index baru

--- api/index.php
+++ api/index.php
@@ -1,16 +1,24 @@
 <?php
 
-// 1. Arahkan folder cache Blade & Storage ke folder /tmp Vercel
-$_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
-$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
-$_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
-$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
-$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+// 1. Arahkan semua folder temp & cache Laravel ke folder /tmp bawaan Serverless
+$_ENV['APP_STORAGE'] = '/tmp/storage';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/storage/framework/config.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/storage/framework/routes.php';
 
-// 2. Buat folder tmp jika belum ada
-if (!is_dir('/tmp/views')) {
-    mkdir('/tmp/views', 0755, true);
+// 2. Buat direktori temporary yang dibutuhkan Laravel
+$dirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/logs',
+];
+
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
 }
 
-// 3. Jalankan aplikasi Laravel
+// 3. Panggil index.php utama Laravel
 require __DIR__ . '/../public/index.php';
